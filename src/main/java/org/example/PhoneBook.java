5252d6e224afcfacf6a72213dff0e08d36e29c64
Auronus <auronus@mail.ru>
2024-10-03 10:56:44 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class PhoneBook {
+
+    public int add() {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add();
+        Assertions.assertEquals(phoneBook.add(), 1);
+    }
 }
